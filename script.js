82fd723be41da8e4b3ef4a28f0af16d5fb2a2b22
newFaizanAli <fa7711598@gmail.com>
2023-08-20 12:01:02 +0500

    faizan

---
+++ script.js
@@ -0,0 +1,12 @@
+function Connection() {
+  let ele = document.getElementById('Render')
+  let layout = "";
+
+  layout += `<h3>Marvel API App</h3>`;
+  ele.innerHTML = "";
+  ele.innerHTML = layout;
+}
+
+window.addEventListener("load", (event) => {
+//   Connection();
+});
