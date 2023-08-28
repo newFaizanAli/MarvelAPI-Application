b45f32f4c4d88fa4099bcb37136d7830b661ea49
newFaizanAli <fa7711598@gmail.com>
2023-08-28 23:23:55 +0500

    faizan

--- script.js
+++ script.js
@@ -1,79 +1,49 @@
-
-let FetchingData = async(value, offset) => {
+let FetchingData = async (value, offset, limit) => {
   let result = await fetch(
-    `https://gateway.marvel.com/v1/public/${value}?ts=1&limit=20&offset=${offset}&apikey=fc51ecbf9581e40a39dfa4cf69b993a5&hash=0ab8a111519e56ed0624d7f7a9b8fafa`
+    `https://gateway.marvel.com/v1/public/${value}?ts=1&limit=${limit}&offset=${offset}&apikey=fc51ecbf9581e40a39dfa4cf69b993a5&hash=0ab8a111519e56ed0624d7f7a9b8fafa`
   ).then((res) => res.json());
-  return result
-}
+  return result;
+};
 
 //Search Data
-let SearchData = async (event) => {
-  let selected = document.getElementById("select").value;
+let SearchData = async () => {
   let input = document.getElementById("input_value").value.toLowerCase();
-  if(selected){  
-    let result = await FetchingData(selected, 0)
+  if (id) {
+    let result = await FetchingData(id, 0, 90);
     let data = result.data.results;
     let serach_data = await data.filter((ele) => {
-      return ele.title ? ele.title.toLowerCase().includes(input) : ele.name.includes(input);
+      return ele.title
+        ? ele.title.toLowerCase().includes(input)
+        : ele.name.includes(input);
     });
     console.log(serach_data);
     RenderElements(serach_data);
-  }else{
-    alert('Kindly select any Option which thing you want to search')
+  } else {
+    alert("Kindly select any Option which thing you want to search");
   }
-  
 };
 
-//Get Data
-let getData = async (offset) => {
+
+//Open Page 
+
+let Open_Page = () => {
   let selected = document.getElementById("select").value;
-  
-  let result = selected ? await FetchingData(selected, offset) : await FetchingData('characters', offset) 
-  console.log(result.data.results);
+  console.log(selected);
+  window.open(`/item.html?id=${selected}`,"_self")
+}
+
+
+//URL Params
+const url = window.location.search;
+console.log(url);
+const urlParams = new URLSearchParams(url);
+const id = urlParams.get('id')
+
+let getData = async () => {
+  let result = await FetchingData(id, 10 , 20)
   RenderElements(result.data.results);
-  //Paginator
-  lastpage = (Math.ceil(result.data.results.total / 20))
-  Paginator()
 };
 
-//Paginator 
-let startIndex = 0
-let endIndex = startIndex + 4
-let lastpage;
-
-let Paginator = () => {
-  let layout = ""
-  layout += `<li class="page-item"><a class="page-link" href="#" onclick="">First</a></li>`;
-  for (var i = startIndex; i <= endIndex; i++) {
-    if (i == endIndex) {
-      layout += `<li class="page-item"><a class="page-link" id="${i}" href="#" onclick="OpenPage(${i}); open_page(${i});">${i}</a></li>`;
-    }
-    else if (i == startIndex) {
-        if (startIndex == 1) {
-            layout += `<li class="page-item"><a class="page-link" id="${i}" href="#" onclick="OpenPage(${i});">${i}</a></li>`;
-        } else {
-          layout += `<li class="page-item"><a class="page-link" id="${i}" href="#" onclick="prev(); OpenPage(${i});">${i}</a></li>`;
-        }
-    }
-    else {
-      layout += `<li class="page-item"><a class="page-link" id="${i}" href="#" onclick="OpenPage(${i});">${i}</a></li>`;
-    }
-}
-  layout += `<li class="page-item"><a class="page-link" href="#" onclick="">last</a></li>`;
-  document.getElementById('pagination').innerHTML = layout;
-}
-
-
-let OpenPage = (i) => {
-  let page = (i - 1) * 20;
-  console.log(page++);
-  if(i === endIndex){
-    startIndex = startIndex + 4
-    endIndex = startIndex + 4
-    getData(page++)
-  }
-  getData(page++)
-}
 
 //Render Elements
 let RenderElements = async (res) => {
@@ -83,19 +53,18 @@ let RenderElements = async (res) => {
   layout += `<div class="row m-0 p-0 justify-content-center">`;
 
   await res.forEach((ele) => {
-    layout += `<div class="card m-3 p-1 shadow-lg"  style="width: 18rem;">`;
+    layout += `<div class="card m-3 p-1 shadow-lg"  style="width: 18rem; height:auto">`;
     layout += `<img src="${ele.thumbnail.path}.jpg" class="card-img-top border rounded-4" alt="..." style="height:50%; overflow: hidden";>`;
     layout += `<div class="card-body">`;
     layout += `<h5 class="card-title">${ele.name ? ele.name : ele.title}</h5>`;
-    layout += `<p class="card-text">${ele.description}</p>`;
-    layout += `<p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>`;
-    
+    layout += `<p class="card-text">${
+      ele.description ? ele.description.slice(1, 80) : `Description not found`
+    }....</p>`;
     layout += `</div>`;
     layout += `<div class="d-flex justify-content-center align-self-center">`;
     layout += `<button type="button" class="btn btn-warning ">More Info</button>`;
     layout += `</div>`;
     layout += ` </div>`;
-    
   });
 
   layout += `</div>`;
@@ -104,6 +73,55 @@ let RenderElements = async (res) => {
   ele.innerHTML = layout;
 };
 
+
+let Set_Slider = () => {
+  var splide = new Splide('.splide', {
+    type: 'loop',
+    perPage: 4,
+    lazyLoad: 'nearby',
+    perMove: 1,
+    breakpoints: {
+      400: {
+        perPage: 1,
+      },
+      800: {
+        perPage: 2,
+      }
+    },
+  });
+
+  splide.mount();
+}
+
+let Card_Slider = async (ElementID, value) => {
+  let ele = document.getElementById(ElementID);
+  let res = await FetchingData(value, 20, 6)
+  .then((res) => {return res.data.results})
+  console.log(res);
+    // .then((res) => console.log(res));
+  let layout = "";
+  await res.forEach((ele) => {
+    layout += `<li class="splide__slide" id="splide__slide">`;
+    layout += ` <div class="card m-2" style="width: 18rem;">`;
+    layout += `<div class="card-body">`;
+    layout += `<img src="${ele.thumbnail.path}.jpg" class="card-img-top border rounded-4" alt="..."
+    style="height:50%; overflow: hidden";>`
+    layout += `<h5 class="card-title text-center">${ele.name}</h5>`;
+    layout += ` </div>`;
+    layout += `</div>`;
+    layout += `</li>`;
+  });
+
+  ele.innerHTML = layout;
+  //Set Slider
+  Set_Slider()
+
+};
+
+let Load_Characters = () => {
+  Card_Slider("Character_list" ,"characters");
+};
+
 window.addEventListener("load", (event) => {
-  getData(0);
+  id ? getData() : Load_Characters()
 });
